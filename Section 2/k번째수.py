a5366ca731b569bf865929fe62991a4c7d5acbc8
Jonnie Yoon <yoonjeongin@sookmyung.ac.kr>
2021-12-08 23:46:17 +0900

    2021.12.08: cannot understand the question

--- Section 2/k번째수.py
+++ Section 2/k번째수.py
@@ -3,3 +3,10 @@ sys.stdin=open("/Users/jeongin/Desktop/Algorithm/python_algorithm/Section 2/inpu
 
 # N개의 숫자로 이루어진 숫자열이 주어지면 해당 숫자열중에서 s번째부터 e번째 까지의 수를
 # 오름차순 정렬했을 때 k번째로 나타나는 숫자를 출력하는 프로그램을 작성하세요.
+
+T = int (input()) #케이스 개수
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    a = list(map(int, input().split())) #읽어서 리스트화
+    print(a)
+    a = a[s-1:e]
